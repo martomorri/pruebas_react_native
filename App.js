3e17ab35a0afde8eb130096fde421e9c6dcb7896
Martin Morrison <martomorr@gmail.com>
2023-06-02 20:41:35 -0300

    creando un componente

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView } from 'react-native';
+import AguanteBoca from './components/AguanteBoca';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aguante Boca 💙💛💙</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <AguanteBoca />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
   },
 });
